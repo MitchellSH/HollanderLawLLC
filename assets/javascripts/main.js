917215db0612ca5d5d1ac1f2b382cd461dc0a0a2
Mitchell Hollander <mitchell.hollander@cybera.net>
2017-05-02 17:17:37 -0500

    kind of almost done

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,4 +1,23 @@
-var string = "THIS HAS BEEN CHANGED";
-var className = "rss-subscribe";
-document.getElementsByClassName(className).innerHTML = string;
-console.log("Script is running");
+var SCREEN_HEIGHT = window.innerHeight;
+
+$(document).ready(initialize);
+
+function initialize(){
+  $('.button-collapse').sideNav();
+  $('.parallax').parallax();
+  $("#index-banner").css("min-height", SCREEN_HEIGHT);
+  $('.smoothScroll').click(smoothScroll);
+}
+
+function smoothScroll(){
+  if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 2000);
+      return false;
+    }
+  }
+}
