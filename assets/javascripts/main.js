99a27f37e4a080ca1363908d9c5083e946895ba2
Mitchell Hollander <mitchell.hollander@cybera.net>
2017-05-04 17:57:32 -0500

    added a contact form, and it works amazingly I might add

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -7,6 +7,34 @@ function initialize(){
   $('.parallax').parallax();
   $("#index-banner").css("min-height", SCREEN_HEIGHT);
   $('.smoothScroll').click(smoothScroll);
+  initWaypoints();
+  $('.intro').addClass('animated fadeInDown');
+  $('.intro-des').addClass('animated fadeInUp');
+  sendEmail();
+}
+
+function sendEmail(){
+  var cF = $('#contactform');
+  cF.submit(function(e) {
+  	e.preventDefault();
+  	$.ajax({
+  		url: '//formspree.io/mitchellshollander@gmail.com',
+  		method: 'POST',
+  		data: $(this).serialize(),
+  		dataType: 'json',
+  		beforeSend: function() {
+  			cF.append('<div class="alert alert--loading">Sending message…</div>');
+  		},
+  		success: function(data) {
+  			cF.find('.alert--loading').hide();
+  			cF.append('<div class="alert alert--success">Message sent!</div>');
+  		},
+  		error: function(err) {
+  			cF.find('.alert--loading').hide();
+  			cF.append('<div class="alert alert--error">Ops, there was an error.</div>');
+  		}
+  	});
+  });
 }
 
 function smoothScroll(){
@@ -20,4 +48,64 @@ function smoothScroll(){
       return false;
     }
   }
+}
+
+function initWaypoints(){
+
+  $('.wp1').waypoint(function() {
+        $('.wp1').addClass('animated fadeInLeft');
+    }, {
+        offset: '70%'
+    });
+    $('.wp2').waypoint(function() {
+        $('.wp2').addClass('animated fadeIn');
+    }, {
+        offset: '65%'
+    });
+    $('.wp3').waypoint(function() {
+        $('.wp3').addClass('animated fadeInRight');
+    }, {
+        offset: '60%'
+    });
+    $('.wp4').waypoint(function() {
+        $('.wp4').addClass('animated fadeIn');
+    }, {
+        offset: '75%'
+    });
+    $('.wp5').waypoint(function() {
+        $('.wp5').addClass('animated fadeIn');
+    }, {
+        offset: '75%'
+    });
+    $('.wp6').waypoint(function() {
+        $('.wp6').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp7').waypoint(function() {
+        $('.wp7').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp8').waypoint(function() {
+        $('.wp8').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp9').waypoint(function() {
+        $('.wp9').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp10').waypoint(function() {
+        $('.wp10').addClass('animated fadeIn');
+    }, {
+        offset: '75%'
+    });
+    $('.wp11').waypoint(function() {
+        $('.wp11').addClass('animated fadeIn');
+    }, {
+        offset: '75%'
+    });
+
 }
